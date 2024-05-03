14fd603a5403a03d406c65c94b3431a855eb0aa4
SanaMnasri <mnasrisana89@gmail.com>
2024-05-03 22:10:40 +0100

    add claas setup

--- src/test/java/com/e2eTest/automation/RunWebSuiteTest.java
+++ src/test/java/com/e2eTest/automation/RunWebSuiteTest.java
@@ -1,5 +1,27 @@
 package com.e2eTest.automation;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+
+/**
+ * The Test Runner File uses the @RunWith() Annotation from JUnit for executing tests.
+ */
+@RunWith(Cucumber.class)
+/**
+ * The @CucumberOptions Annotation is used to define to location of feature files, step definitions, reporting
+ * integration
+ */
+@CucumberOptions(features = { "src/specs/features" }, plugin = { "pretty",
+		"htmL:target/cucumber-report.html" }, tags = (" @login_cas_passant"), monochrome = true, snippets = SnippetType.CAMELCASE
+
+)
+/**
+ * This class is used to run the test, which is JUnit Test Runner Class containing the Step Definition location and 
+ * the other primary metadata required to run the test.
+ */
 public class RunWebSuiteTest {
 
 }
