201b1ecff4e598a440b2dc2401721037aa96f412
SanaMnasri <mnasrisana89@gmail.com>
2024-05-07 00:24:57 +0100

    update project

--- src/test/java/com/e2eTest/automation/RunWebSuiteTest.java
+++ src/test/java/com/e2eTest/automation/RunWebSuiteTest.java
@@ -7,23 +7,24 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType;
 
 /**
- * The Test Runner File uses the @RunWith() Annotation from JUnit for executing tests.
+ * The Test Runner File uses the @RunWith() Annotation from JUnit for executing
+ * tests.
  */
 @RunWith(Cucumber.class)
 /**
- * The @CucumberOptions Annotation is used to define to location of feature files, step definitions, reporting
- * integration
+ * The @CucumberOptions Annotation is used to define to location of feature
+ * files, step definitions, reporting integration
  */
 @CucumberOptions(features = { "src/specs/features" }, plugin = { "pretty",
-		"htmL:target/cucumber-report.html" }, tags = (" @login_cas_passant"),
-		//glue{},
-		monochrome = true,
-		snippets = SnippetType.CAMELCASE
+		"html: target/cucumber_report.html" }, tags = (" @login_cas_passant"),
+		// glue{},
+		monochrome = true, snippets = SnippetType.CAMELCASE
 
 )
 /**
- * This class is used to run the test, which is JUnit Test Runner Class containing the Step Definition location and
- * the other primary metadata required to run the test.
+ * This class is used to run the test, which is JUnit Test Runner Class
+ * containing the Step Definition location and the other primary metadata
+ * required to run the test.
  */
 public class RunWebSuiteTest {
 
---
+++ src/test/java/com/e2eTest/automation/step_definitions/LoginStepDefinition.java
@@ -0,0 +1,33 @@
+package com.e2eTest.automation.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinition {
+
+
+	@Given("Je me connecte sur l application SWAGLABSS")
+	public void jeMeConnecteSurLApplicationSWAGLABSS() {
+	   
+	}
+	@When("Je saisis le user name {string}")
+	public void jeSaisisLeUserName(String string) {
+	   
+	}
+	@When("Je saisis le mot de passe {string}")
+	public void jeSaisisLeMotDePasse(String string) {
+
+	}
+	@When("Je clique sur le bouton login")
+	public void jeCliqueSurLeBoutonLogin() {
+	   
+	}
+	@Then("Je me rederige vers la page d acceuil {string}")
+	public void jeMeRederigeVersLaPageDAcceuil(String string) {
+	   
+
+
+
+
+}
